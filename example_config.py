e78ca3e2568eae95ba373d89d126c254115a8fe7
Yeinso Blanco <yblanco@bitinka.com>
2018-09-21 15:58:13 -0500

    Modifying config_example

--- example_config.py
+++ example_config.py
@@ -4,12 +4,17 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    PORT = 3002
+    HOST = '0.0.0.0'
  
 class Development(Config):
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave'
+    ENVIROMENT = 'Development'
+
 class Testing(Config):
     TESTING = True
+    ENVIROMENT = 'Testing'
  
 class Production(Config):
     pass    
